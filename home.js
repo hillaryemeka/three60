d764264cdbea58acadc2c7019fba51e2730e4e00
Hillary <odohilaryemeka@gmail.com>
2022-07-16 10:06:09 +0100

    home page completed

--- home.js
+++ home.js
@@ -12,11 +12,27 @@ function displayUserDetails() {
   }
 }
 
+
+
 function logout() {
   localStorage.removeItem('user')
   localStorage.removeItem('token')
   // localStorage.clear()
   window.location.href = "sign_in.html"
+}
+
+function displayUser() {
+  const user = JSON.parse(localStorage.getItem("user"))
+  const token = localStorage.getItem("token")
+  let displayBlock = document.getElementById('user-details').style.display
+  document.getElementById('hello-user').innerText = user.firstName +" "+ user.lastName
+  document.getElementById('user-email').innerText = user.email
+
+  if (displayBlock == "none") {
+    document.getElementById('user-details').style.display = "flex"
+  } else{
+    document.getElementById('user-details').style.display = "none"
+  }
 }
 
 displayUserDetails()
